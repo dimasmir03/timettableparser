431234c76dadfa5bd241e5d815b5521b57292932
dimasmir03 <saenkodmitriiol@gmail.com>
2023-07-06 11:30:08 +0300

    asd

--- index.js
+++ index.js
@@ -2,27 +2,34 @@ var PORT = process.env.PORT || 3030;
 var express = require("express");
 var app = express();
 var server = require("http").createServer(app);
-var axios = require("axios")
+const cheerio = require('cheerio')
+const fetch = require('node-fetch');
+
+var parse = require('node-html-parser');
 
 app.get("/group",(req,res)=>{
-    res.send("group");
-    console.log("group:");
-    console.log(req.params.name);
+    //console.log(req.params.name);
 })
+
 app.get("/",async (req,res)=>{
-    
     console.log("main:");
-    console.log(req.query.name)
 
-    var result = await axios.post('https://',{
-    params: {
-        tab: '7',
-        gp_name: '2бАСУ2',
-        gp_id: '8700',
-    }})
+    headers= {
+        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+    }
+    const data = {
+        'tab': '7',
+        'gp_name': '2бАСУ2',
+        'gp_id': '8700'
+    }
+    domain = "https://raspisanie.madi.ru/tplan/tasks/tableFiller.php"
 
-    res.send(result);
+    var result = await fetch(domain,{
+        method: 'POST',
+        body: new URLSearchParams(data),
+        headers: headers
+        })
+    res.send(await result.text())
 })
-
 
 server.listen(PORT,"")
